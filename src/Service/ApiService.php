52cd9a5ab286f2ad84f54a7b820acdd512c72479
Mohammed Bensaad <bensaadmucret@gmail.com>
2022-05-31 13:55:51 +0200

    📦 NEW: add api key in env

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -8,22 +8,22 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class ApiService
 {
     private $client;
-
-    public function __construct(HttpClientInterface $client)
+    public string $apiKey;
+    public function __construct(HttpClientInterface $client, $apiKey)
     {
         $this->client = $client;
+        $this->apiKey = $apiKey;
     }
-
+  
     public function getApiData()
     {
-
-
+       
         $response = $this->client->request(
             'GET',
             'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
             [
                 'headers' => [
-                    'X-CMC_PRO_API_KEY' => 'afc02c85-196c-4b82-8bf8-8ce9d1a83d1a',
+                    'X-CMC_PRO_API_KEY' => $this->apiKey
                 ],
                 'query' => [
                     'start' => 1,
@@ -36,17 +36,12 @@ class ApiService
         );
         $statusCode = $response->getStatusCode();
         $content = $response->getContent();
-
         $content = $response->toArray();
        
-
-     
         if(empty($content)){
             throw new \Exception('Erreur dans le service ApiService');
         }
-
         return $content;
-
     }
        
 }
