1d03f7d6a6eac72b013edbd5631dbc62bb42e029
Bensaad mohammed <bensaadmucret@gmail.com>
2022-06-10 22:20:20 +0200

    📦 NEW: structure dir/js

---
+++ public/js/calc.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', () => {
+    //alert('Hello World!');
+    const quantity = document.getElementById('form_quantity');
+    const price = document.getElementById('form_price');
+    const crypto = document.getElementById('form_name');
+    
+   if (quantity && crypto) {
+         quantity.addEventListener('input', () => {
+            const quantityValue = quantity.value;
+            const priceValue = price.value;
+            const cryptoValue = crypto.value;
+            const total = quantityValue * cryptoValue;
+            price.value = total;
+        crypto.addEventListener('change', () => {
+            const quantityValue = quantity.value;
+            const priceValue = price.value;
+            const cryptoValue = crypto.value;
+            const total = quantityValue / cryptoValue;
+            price.value = total;
+        });
+    });
+}
+
+});
